5117b9f9a8bdb904fdb913a64098a0f6e6d64a59
unknown <charly.jason1940@gmail.com>
2020-10-25 20:50:31 -0600

    Proyecto1

---
+++ agents/BookBuyerAgent.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agents;
+
+/**
+ *
+ * @author 52341
+ */
+
+import behaviours.RequestPerformer;
+import jade.core.AID;
+import jade.core.behaviours.*;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+import jade.core.Agent;
+
+public class BookBuyerAgent extends Agent {
+    
+    
+    private String bookTitle;
+  private AID[] sellerAgents;
+  private int ticker_timer = 10000;
+  private BookBuyerAgent this_agent = this;
+  
+  protected void setup() {
+    System.out.println("Buyer agent " + getAID().getName() + " is ready");
+    
+    Object[] args = getArguments();
+    if(args != null && args.length > 0) {
+      bookTitle = (String)args[0];
+      System.out.println("Book: " + bookTitle);
+      
+      addBehaviour(new TickerBehaviour(this, ticker_timer) {
+        protected void onTick() {
+          System.out.println("Trying to buy " + bookTitle);
+          
+          DFAgentDescription template = new DFAgentDescription();
+          ServiceDescription sd = new ServiceDescription();
+          sd.setType("book-selling");
+          template.addServices(sd);
+          
+          try {
+            DFAgentDescription[] result = DFService.search(myAgent, template);
+            System.out.println("Found the following seller agents:");
+            sellerAgents = new AID[result.length];
+            for(int i = 0; i < result.length; i++) {
+              sellerAgents[i] = result[i].getName();
+              System.out.println(sellerAgents[i].getName());
+            }
+            
+          }catch(FIPAException fe) {
+            fe.printStackTrace();
+          }
+          
+          myAgent.addBehaviour(new RequestPerformer(this_agent));
+        }
+      });
+    } else {
+      System.out.println("No target book title specified");
+      doDelete();
+    }
+  }
+  
+  protected void takeDown() {
+    System.out.println("Buyer agent " + getAID().getName() + " terminating");
+  }
+  
+  public AID[] getSellerAgents() {
+    return sellerAgents;
+  }
+  
+  public String getBookTitle() {
+    return bookTitle;
+  }
+
+    public void updateCatalogue(String title, int parseInt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
